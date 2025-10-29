724fe98bc317b14c3b70008dfa8d2b40b9669dfc
Rafael <rafaelalimaaa79@gmail.com>
2025-10-28 23:54:21 -0700

    feat: add PostHog integration for analytics and update environment configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 import * as Sentry from "@sentry/react";
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App.tsx'
 import './index.css'
 
@@ -19,4 +21,18 @@ Sentry.init({
   enableLogs: true
 });
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true,
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <App />
+    </PostHogProvider>
+  </React.StrictMode>
+)
